fd8b23e93044b4e1e32b656225d70a4bcef300e7
gar3thcar3y <garethcarey366@gmail.com>
2024-07-31 22:05:41 +0100

    change grid size

--- code.js
+++ code.js
@@ -1,18 +1,34 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.style.display = "flex";
-    row.style.justifyContent = "center";
-    console.log("test");
 
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div")
-        div.classList.add("squere")
-        row.appendChild(div);
+function create_grid(num) {
+    $("#container").innerHTML = ""
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement("div");
+        row.style.display = "flex";
+        row.style.justifyContent = "center";
+
+        for (let j = 0; j < num; j++) {
+            var id = i + "," + j
+            const div = document.createElement("div")
+            div.classList.add("squere")
+            div.id = id
+            div.onclick = "color('" + i + "," + j + "');"
+            row.appendChild(div);
+        }
+
+        $("#container").appendChild(row);
     }
-
-    $("#container").appendChild(row);
 }
 
+create_grid(16);
+
+function new_width() {
+    var width = window.prompt();
+    while(width > 100){
+        width = window.prompt();
+    }
+    create_grid(width);
+
+}
